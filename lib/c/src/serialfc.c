fc9d673dfe8c7d54548f9e0eb06948b5a09e65b6
William Fagan <willf@commtech-fastcom.com>
2013-01-09 16:48:11 -0600

    Add C library.

---
+++ lib/c/src/serialfc.c
@@ -0,0 +1,336 @@
+/*! \file */ 
+#include <stdio.h>
+
+#include "serialfc.h"
+
+#define MAX_NAME_LENGTH 25
+
+/******************************************************************************/
+/*!
+
+  \brief Opens a handle to a SerialFC port.
+
+  \param[in] port_num 
+    the SerialFC port number
+  \param[in] overlapped 
+    whether you would like to use the port in overlapped mode
+  \param[out] h 
+    user variable that the port's HANDLE will be assigned to
+      
+  \return 0 
+    if the operation completed successfully
+  \return >= 1 
+    if the operation failed (see MSDN 'System Error Codes')
+
+  \note
+    Opening a handle using this API will only give you access to the
+	asynchronous functionality of the card. You will need to use the FSCC ports
+	if you would like to use the synchronous functionality.
+
+*/
+/******************************************************************************/
+int serialfc_connect(unsigned port_num, BOOL overlapped, HANDLE *h)
+{
+  char name[MAX_NAME_LENGTH];
+  DWORD flags_and_attributes = FILE_ATTRIBUTE_NORMAL;
+
+  sprintf_s(name, MAX_NAME_LENGTH, "\\\\.\\COM%u", port_num);
+        
+  if (overlapped)
+  	flags_and_attributes |= FILE_FLAG_OVERLAPPED;
+
+  *h = CreateFile(name,
+  		GENERIC_READ | GENERIC_WRITE,
+  		FILE_SHARE_READ | FILE_SHARE_WRITE,
+  		NULL,
+  		OPEN_EXISTING,
+  		flags_and_attributes,
+  		NULL
+  );
+
+  return (*h != INVALID_HANDLE_VALUE) ? ERROR_SUCCESS : GetLastError();
+}
+
+int serialfc_enable_rs485(HANDLE h)
+{
+  DWORD temp;
+  BOOL result;
+
+  result = DeviceIoControl(h, (DWORD)IOCTL_FASTCOM_ENABLE_RS485, 
+                           NULL, 0, 
+                           NULL, 0, 
+                           &temp, (LPOVERLAPPED)NULL);
+
+  return (result == TRUE) ? ERROR_SUCCESS : GetLastError();
+}
+
+int serialfc_disable_rs485(HANDLE h)
+{
+  DWORD temp;
+  BOOL result;
+
+  result = DeviceIoControl(h, (DWORD)IOCTL_FASTCOM_DISABLE_RS485, 
+                           NULL, 0, 
+                           NULL, 0, 
+                           &temp, (LPOVERLAPPED)NULL);
+
+  return (result == TRUE) ? ERROR_SUCCESS : GetLastError();
+}
+
+int serialfc_get_rs485(HANDLE h, BOOL *status)
+{
+  DWORD temp;
+  BOOL result;
+
+  result = DeviceIoControl(h, (DWORD)IOCTL_FASTCOM_GET_RS485, 
+                           NULL, 0, 
+                           status, sizeof(*status), 
+                           &temp, (LPOVERLAPPED)NULL);
+
+  return (result == TRUE) ? ERROR_SUCCESS : GetLastError();
+}
+
+int serialfc_enable_echo_cancel(HANDLE h)
+{
+  DWORD temp;
+  BOOL result;
+
+  result = DeviceIoControl(h, (DWORD)IOCTL_FASTCOM_ENABLE_ECHO_CANCEL, 
+                           NULL, 0, 
+                           NULL, 0, 
+                           &temp, (LPOVERLAPPED)NULL);
+
+  return (result == TRUE) ? ERROR_SUCCESS : GetLastError();
+}
+
+int serialfc_disable_echo_cancel(HANDLE h)
+{
+  DWORD temp;
+  BOOL result;
+
+  result = DeviceIoControl(h, (DWORD)IOCTL_FASTCOM_DISABLE_ECHO_CANCEL, 
+                           NULL, 0, 
+                           NULL, 0, 
+                           &temp, (LPOVERLAPPED)NULL);
+
+  return (result == TRUE) ? ERROR_SUCCESS : GetLastError();
+}
+
+int serialfc_get_echo_cancel(HANDLE h, BOOL *status)
+{
+  DWORD temp;
+  BOOL result;
+
+  result = DeviceIoControl(h, (DWORD)IOCTL_FASTCOM_GET_ECHO_CANCEL, 
+                           NULL, 0, 
+                           status, sizeof(*status), 
+                           &temp, (LPOVERLAPPED)NULL);
+
+  return (result == TRUE) ? ERROR_SUCCESS : GetLastError();
+}
+
+int serialfc_enable_termination(HANDLE h)
+{
+  DWORD temp;
+  BOOL result;
+
+  result = DeviceIoControl(h, (DWORD)IOCTL_FASTCOM_ENABLE_TERMINATION, 
+                           NULL, 0, 
+                           NULL, 0, 
+                           &temp, (LPOVERLAPPED)NULL);
+
+  return (result == TRUE) ? ERROR_SUCCESS : GetLastError();
+}
+
+int serialfc_disable_termination(HANDLE h)
+{
+  DWORD temp;
+  BOOL result;
+
+  result = DeviceIoControl(h, (DWORD)IOCTL_FASTCOM_DISABLE_TERMINATION, 
+                           NULL, 0, 
+                           NULL, 0, 
+                           &temp, (LPOVERLAPPED)NULL);
+
+  return (result == TRUE) ? ERROR_SUCCESS : GetLastError();
+}
+
+int serialfc_get_termination(HANDLE h, BOOL *status)
+{
+  DWORD temp;
+  BOOL result;
+
+  result = DeviceIoControl(h, (DWORD)IOCTL_FASTCOM_GET_TERMINATION, 
+                           NULL, 0, 
+                           status, sizeof(*status), 
+                           &temp, (LPOVERLAPPED)NULL);
+
+  return (result == TRUE) ? ERROR_SUCCESS : GetLastError();
+}
+
+int serialfc_set_sample_rate(HANDLE h, unsigned rate)
+{
+  DWORD temp;
+  BOOL result;
+
+  result = DeviceIoControl(h, (DWORD)IOCTL_FASTCOM_SET_SAMPLE_RATE, 
+                           &rate, sizeof(rate), 
+                           NULL, 0, 
+                           &temp, (LPOVERLAPPED)NULL);
+
+  return (result == TRUE) ? ERROR_SUCCESS : GetLastError();
+}
+
+int serialfc_get_sample_rate(HANDLE h, unsigned *rate)
+{
+  DWORD temp;
+  BOOL result;
+
+  result = DeviceIoControl(h, (DWORD)IOCTL_FASTCOM_GET_SAMPLE_RATE, 
+                           NULL, 0, 
+                           rate, sizeof(*rate), 
+                           &temp, (LPOVERLAPPED)NULL);
+
+  return (result == TRUE) ? ERROR_SUCCESS : GetLastError();
+}
+
+int serialfc_set_tx_trigger(HANDLE h, unsigned level)
+{
+  DWORD temp;
+  BOOL result;
+
+  result = DeviceIoControl(h, (DWORD)IOCTL_FASTCOM_SET_TX_TRIGGER, 
+                           &level, sizeof(level), 
+                           NULL, 0, 
+                           &temp, (LPOVERLAPPED)NULL);
+
+  return (result == TRUE) ? ERROR_SUCCESS : GetLastError();
+}
+
+int serialfc_get_tx_trigger(HANDLE h, unsigned *level)
+{
+  DWORD temp;
+  BOOL result;
+
+  result = DeviceIoControl(h, (DWORD)IOCTL_FASTCOM_GET_TX_TRIGGER, 
+                           NULL, 0, 
+                           level, sizeof(*level), 
+                           &temp, (LPOVERLAPPED)NULL);
+
+  return (result == TRUE) ? ERROR_SUCCESS : GetLastError();
+}
+
+int serialfc_set_rx_trigger(HANDLE h, unsigned level)
+{
+  DWORD temp;
+  BOOL result;
+
+  result = DeviceIoControl(h, (DWORD)IOCTL_FASTCOM_SET_RX_TRIGGER, 
+                           &level, sizeof(level), 
+                           NULL, 0, 
+                           &temp, (LPOVERLAPPED)NULL);
+
+  return (result == TRUE) ? ERROR_SUCCESS : GetLastError();
+}
+
+int serialfc_get_rx_trigger(HANDLE h, unsigned *level)
+{
+  DWORD temp;
+  BOOL result;
+
+  result = DeviceIoControl(h, (DWORD)IOCTL_FASTCOM_GET_RX_TRIGGER, 
+                           NULL, 0, 
+                           level, sizeof(*level), 
+                           &temp, (LPOVERLAPPED)NULL);
+
+  return (result == TRUE) ? ERROR_SUCCESS : GetLastError();
+}
+
+ 
+/******************************************************************************/
+/*!
+
+  \brief Transmits data out of a port.
+
+  \param[in] h 
+    HANDLE to the port
+  \param[in] buf
+    the buffer containing the data to transmit
+  \param[in] size
+    the number of bytes to transmit from 'buf'
+  \param[out] bytes_written
+    the input variable to store how many bytes were actually written
+  \param[in,out] o
+    OVERLAPPED structure for asynchronous operation
+      
+  \return 0 
+    if the operation completed successfully
+  \return >= 1 
+    if the operation failed (see MSDN 'System Error Codes')
+
+*/
+/******************************************************************************/
+int serialfc_write(HANDLE h, char *buf, unsigned size, unsigned *bytes_written, OVERLAPPED *o)
+{
+  BOOL result;
+        
+  result = WriteFile(h, buf, size, (DWORD*)bytes_written, o);
+
+  return (result == TRUE) ? ERROR_SUCCESS : GetLastError();
+}
+
+/******************************************************************************/
+/*!
+
+  \brief Reads data out of a port.
+
+  \param[in] h 
+    HANDLE to the port
+  \param[in] buf
+    the input buffer used to store the receive data
+  \param[in] size
+    the maximum number of bytes to read in (typically sizeof(buf))
+  \param[out] bytes_read
+    the user variable to store how many bytes were actually read
+  \param[in,out] o
+    OVERLAPPED structure for asynchronous operation
+      
+  \return 0 
+    if the operation completed successfully
+  \return >= 1 
+    if the operation failed (see MSDN 'System Error Codes')
+
+*/
+/******************************************************************************/
+int serialfc_read(HANDLE h, char *buf, unsigned size, unsigned *bytes_read, OVERLAPPED *o)
+{
+  BOOL result;
+
+  result = ReadFile(h, buf, size, (DWORD*)bytes_read, o);
+
+  return (result == TRUE) ? ERROR_SUCCESS : GetLastError();
+}
+
+/******************************************************************************/
+/*!
+
+  \brief Closes the handle to a SerialFC port.
+
+  \param[in] h 
+    HANDLE to the port
+      
+  \return 0 
+    if closing the port completed successfully
+  \return >= 1 
+    if the operation failed (see MSDN 'System Error Codes')
+
+*/
+/******************************************************************************/
+int serialfc_disconnect(HANDLE h)
+{
+	BOOL result;
+
+	result = CloseHandle(h);
+
+	return (result == TRUE) ? ERROR_SUCCESS : GetLastError();
+}
---
+++ lib/c/src/serialfc.h
@@ -0,0 +1,61 @@
+/*! \file */ 
+
+#ifndef SERIALFC_H
+#define SERIALFC_H
+
+#ifdef __cplusplus
+extern "C" 
+{
+#endif
+
+#include <Windows.h>
+
+#define SERIALFC_IOCTL_MAGIC 0x8019
+
+#define IOCTL_FASTCOM_ENABLE_RS485 CTL_CODE(SERIALFC_IOCTL_MAGIC, 0x800, METHOD_BUFFERED, FILE_ANY_ACCESS)
+#define IOCTL_FASTCOM_DISABLE_RS485 CTL_CODE(SERIALFC_IOCTL_MAGIC, 0x801, METHOD_BUFFERED, FILE_ANY_ACCESS)
+#define IOCTL_FASTCOM_GET_RS485 CTL_CODE(SERIALFC_IOCTL_MAGIC, 0x802, METHOD_BUFFERED, FILE_ANY_ACCESS)
+
+#define IOCTL_FASTCOM_ENABLE_ECHO_CANCEL CTL_CODE(SERIALFC_IOCTL_MAGIC, 0x803, METHOD_BUFFERED, FILE_ANY_ACCESS)
+#define IOCTL_FASTCOM_DISABLE_ECHO_CANCEL CTL_CODE(SERIALFC_IOCTL_MAGIC, 0x804, METHOD_BUFFERED, FILE_ANY_ACCESS)
+#define IOCTL_FASTCOM_GET_ECHO_CANCEL CTL_CODE(SERIALFC_IOCTL_MAGIC, 0x805, METHOD_BUFFERED, FILE_ANY_ACCESS)
+
+#define IOCTL_FASTCOM_ENABLE_TERMINATION CTL_CODE(SERIALFC_IOCTL_MAGIC, 0x806, METHOD_BUFFERED, FILE_ANY_ACCESS)
+#define IOCTL_FASTCOM_DISABLE_TERMINATION CTL_CODE(SERIALFC_IOCTL_MAGIC, 0x807, METHOD_BUFFERED, FILE_ANY_ACCESS)
+#define IOCTL_FASTCOM_GET_TERMINATION CTL_CODE(SERIALFC_IOCTL_MAGIC, 0x808, METHOD_BUFFERED, FILE_ANY_ACCESS)
+
+#define IOCTL_FASTCOM_SET_SAMPLE_RATE CTL_CODE(SERIALFC_IOCTL_MAGIC, 0x809, METHOD_BUFFERED, FILE_ANY_ACCESS)
+#define IOCTL_FASTCOM_GET_SAMPLE_RATE CTL_CODE(SERIALFC_IOCTL_MAGIC, 0x80A, METHOD_BUFFERED, FILE_ANY_ACCESS)
+
+#define IOCTL_FASTCOM_SET_TX_TRIGGER CTL_CODE(SERIALFC_IOCTL_MAGIC, 0x80B, METHOD_BUFFERED, FILE_ANY_ACCESS)
+#define IOCTL_FASTCOM_GET_TX_TRIGGER CTL_CODE(SERIALFC_IOCTL_MAGIC, 0x80C, METHOD_BUFFERED, FILE_ANY_ACCESS)
+
+#define IOCTL_FASTCOM_SET_RX_TRIGGER CTL_CODE(SERIALFC_IOCTL_MAGIC, 0x80D, METHOD_BUFFERED, FILE_ANY_ACCESS)
+#define IOCTL_FASTCOM_GET_RX_TRIGGER CTL_CODE(SERIALFC_IOCTL_MAGIC, 0x80E, METHOD_BUFFERED, FILE_ANY_ACCESS)
+
+
+__declspec(dllexport) int serialfc_connect(unsigned port_num, BOOL overlapped, HANDLE *h);
+__declspec(dllexport) int serialfc_enable_rs485(HANDLE h);
+__declspec(dllexport) int serialfc_disable_rs485(HANDLE h);
+__declspec(dllexport) int serialfc_get_rs485(HANDLE h, BOOL *status);
+__declspec(dllexport) int serialfc_enable_echo_cancel(HANDLE h);
+__declspec(dllexport) int serialfc_disable_echo_cancel(HANDLE h);
+__declspec(dllexport) int serialfc_get_echo_cancel(HANDLE h, BOOL *status);
+__declspec(dllexport) int serialfc_enable_termination(HANDLE h);
+__declspec(dllexport) int serialfc_disable_termination(HANDLE h);
+__declspec(dllexport) int serialfc_get_termination(HANDLE h, BOOL *status);
+__declspec(dllexport) int serialfc_set_sample_rate(HANDLE h, unsigned rate);
+__declspec(dllexport) int serialfc_get_sample_rate(HANDLE h, unsigned *rate);
+__declspec(dllexport) int serialfc_set_tx_trigger(HANDLE h, unsigned level);
+__declspec(dllexport) int serialfc_get_tx_trigger(HANDLE h, unsigned *level);
+__declspec(dllexport) int serialfc_set_rx_trigger(HANDLE h, unsigned level);
+__declspec(dllexport) int serialfc_get_rx_trigger(HANDLE h, unsigned *level);
+__declspec(dllexport) int serialfc_write(HANDLE h, char *buf, unsigned size, unsigned *bytes_written, OVERLAPPED *o);
+__declspec(dllexport) int serialfc_read(HANDLE h, char *buf, unsigned size, unsigned *bytes_read, OVERLAPPED *o);
+__declspec(dllexport) int serialfc_disconnect(HANDLE h);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
