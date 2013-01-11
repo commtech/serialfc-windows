edf0b0ac46a497f3d78942a1f16c6abd38829d88
William Fagan <willf@commtech-fastcom.com>
2013-01-11 13:51:03 -0600

    Add libraries to release setup and switch to production.

--- release_driver.bat
+++ release_driver.bat
@@ -13,16 +13,40 @@ mkdir tmp\production\
 :build_drivers
 echo Building Release Drivers...
 start /I /WAIT build_driver.bat "fre x86 WXP" objfre_wxp_x86 i386 production XP_X86
-start /I /WAIT build_driver.bat "chk x64 WNET" objchk_wnet_amd64 amd64 checked Server2003_X64
+start /I /WAIT build_driver.bat "fre x64 WNET" objfre_wnet_amd64 amd64 production Server2003_X64
+
+:build_libs
+echo Building Libraries...
+start /I /WAIT build_libs.bat
 
 :create_directories
 echo Creating Directories...
-for %%A in (32, 64) do mkdir %TOP%\%%A\
+for %%A in (32, 64, lib) do mkdir %TOP%\%%A\
+for %%A in (c, c++, net, python) do mkdir %TOP%\lib\%%A\
+
+:copy_dll_files
+echo Copying DLL Files...
+copy lib\c\cserialfc*.dll %TOP%\lib\c\ > nul
+copy lib\c\cserialfc*.lib %TOP%\lib\c\ > nul
+copy lib\c\src\*.c %TOP%\lib\c\ > nul
+copy lib\c\src\*.h %TOP%\lib\c\ > nul
+copy lib\c\makefile %TOP%\lib\c\ > nul
+copy "lib\c++\cppserialfc*.dll" "%TOP%\lib\c++\" > nul
+copy "lib\c++\cppserialfc*.lib" "%TOP%\lib\c++\" > nul
+copy lib\c\cserialfc*.dll "%TOP%\lib\c++\" > nul
+copy "lib\c++\src\*.cpp" "%TOP%\lib\c++\" > nul
+copy "lib\c++\src\*.hpp" "%TOP%\lib\c++\" > nul
+copy "lib\c++\makefile" "%TOP%\lib\c++\" > nul
+copy lib\net\netserialfc*.dll %TOP%\lib\net\ > nul
+copy lib\c\cserialfc*.dll %TOP%\lib\net\ > nul
+copy lib\net\src\*.cs %TOP%\lib\net\ > nul
+copy lib\net\makefile %TOP%\lib\net\ > nul
+copy lib\python\serialfc.py %TOP%\lib\python\ > nul
 
 :copy_sys_files
 echo Copying Driver Files...
 copy tmp\production\i386\* %TOP%\32\ > nul
-copy tmp\checked\amd64\* %TOP%\64\ > nul
+copy tmp\production\amd64\* %TOP%\64\ > nul
 
 :copy_setup_files
 :echo Copying Setup Files...
