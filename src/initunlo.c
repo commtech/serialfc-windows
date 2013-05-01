28fbb45338c999e2791b58d1d0e822bdbc466d03
William Fagan <willf@commtech-fastcom.com>
2013-05-01 11:05:42 -0500

    Update a print statement for Fastcom branding.

--- src/initunlo.c
+++ src/initunlo.c
@@ -106,7 +106,7 @@ Return Value:
     WPP_INIT_TRACING( DriverObject, RegistryPath );
 
     SerialDbgPrintEx(TRACE_LEVEL_INFORMATION, DBG_INIT,
-                    "Serial Sample (WDF Version) - Built %s %s\n",
+                    "SerialFC (WDF Version) - Built %s %s\n",
                      __DATE__, __TIME__);
     //
     // Register a cleanup callback so that we can call WPP_CLEANUP when
