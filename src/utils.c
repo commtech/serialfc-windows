78dd5b2fbb304fc19526ab2cade4df77a181f133
William Fagan <willf@commtech-fastcom.com>
2013-06-03 12:17:20 -0500

    Remove the auto-disabling of DTR when switching to synchronous mode because it isn't required.

--- src/utils.c
+++ src/utils.c
@@ -4206,17 +4206,6 @@ NTSTATUS FsccDisableAsync(SERIAL_DEVICE_EXTENSION *pDevExt)
     /* UART_{A,B} */
     new_fcr = orig_fcr & ~(0x01000000 << pDevExt->Channel);
 
-    /* FSTDTR{A,B} */
-    switch (pDevExt->Channel) {
-    case 0:
-        new_fcr &= ~0x00080000;
-        break;
-
-    case 1:
-        new_fcr &= ~0x00800000;
-        break;
-    }
-
     WRITE_PORT_ULONG(ULongToPtr(pDevExt->Bar2), new_fcr);
 
     return STATUS_SUCCESS;
