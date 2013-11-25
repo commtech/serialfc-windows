3795bf14b3d87097bbb2a278d3cb9f4a63b03016
William Fagan <willf@commtech-fastcom.com>
2013-11-25 14:16:37 -0600

    Fix script comment.

--- build_libs.bat
+++ build_libs.bat
@@ -4,7 +4,7 @@ echo off
 call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\Tools\vsvars32.bat"
 cd %~dp0
 
-:building_driver
+:building_libs
 echo Building DLLs...
 pushd %TOP%\c\ & nmake clean & nmake & nmake DEBUG & popd > nul
 pushd "%TOP%\c++\" & nmake clean & nmake & nmake DEBUG & popd > nul
